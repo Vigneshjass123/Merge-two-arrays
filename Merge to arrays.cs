243bd8c73ac63235be2538b1786ba3f5f28d0c81
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-04-04 00:34:09 +0530

    Add project files.

---
+++ Merge to arrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_two_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2,3,4 };
+            int[] arr2 = { 5,6,7,8,9};
+            
+                    var myList = new List<int>();
+                    myList.AddRange(arr1);
+                    myList.AddRange(arr2);
+                    int[] c = myList.ToArray();
+                    foreach (int K in c)
+                    {
+                        Console.WriteLine(K);
+                    }
+
+                }
+            }
+        }
+    
+
+
